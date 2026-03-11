d90f29a3ce14d475d785e7e14d7c348acf5909fb
Weiss_GCM <darma20060311@gmail.com>
2026-03-11 07:36:15 +0700

    040326

---
+++ Backend Development/Pertemuan 2 - FP2-040326/express-app/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+
+app get("/", (req, res) => {
+  res.send("Hello Sekai!");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000 Localhost");
+});
