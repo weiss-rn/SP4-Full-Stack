aa79ace1331ddd9e66b239214c60c5f9c2229c5c
Weiss_GCM <darma20060311@gmail.com>
2026-03-11 10:43:49 +0700

    110306

--- Backend Development/Pertemuan 3 - FP3-110326/crud-express-mysql/app.js
+++ Backend Development/Pertemuan 3 - FP3-110326/crud-express-mysql/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const db = require('./db');
+const db = require('../db');
 
 const app = express();
 const port = 3000;
